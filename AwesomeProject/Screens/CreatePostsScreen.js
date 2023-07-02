09d6dd64bd246219a3bfd06b4d1a04d47d7e84e6
NadiaOl <115547708+NadiaOl@users.noreply.github.com>
2023-07-02 21:50:07 +0300

    hw 5 start

--- AwesomeProject/Screens/CreatePostsScreen.js
+++ AwesomeProject/Screens/CreatePostsScreen.js
@@ -1,15 +1,34 @@
-import React, { useState } from "react";
-import { Image, StyleSheet, Text, TextInput, View, KeyboardAvoidingView } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { Image, StyleSheet, Text, TextInput, View, KeyboardAvoidingView, TouchableOpacity } from "react-native";
 import PablicationPhoto from './img/Content.jpg'
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 
 export default function CreatePostsScreen() {
 
     const [name, setName] = useState("");
     const [location, setLocation] = useState("");
     const navigation = useNavigation();
+    const [hasPermission, setHasPermission] = useState(null);
+    const [cameraRef, setCameraRef] = useState(null);
+    const [type, setType] = useState(Camera.Constants.Type.back);
     
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            await MediaLibrary.requestPermissionsAsync();
+            setHasPermission(status === "granted");
+        })();
+    }, []);
+    
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
 
     return (
         <View style={styles.container}>
@@ -17,33 +36,71 @@ export default function CreatePostsScreen() {
                 <Text style={styles.title}>Створити публікацію</Text>
             </View>
             <View style={styles.body}>
-                <Image source={PablicationPhoto}/>
+                <Image source={PablicationPhoto} />
+                <View style={styles.cameraContainer}>
+                    <Camera
+                        style={styles.camera}
+                        type={type}
+                        ref={setCameraRef}
+                        >
+                        <View style={styles.photoView}>
+                            <TouchableOpacity
+                                style={styles.flipContainer}
+                                onPress={() => {
+                                setType(
+                                type === Camera.Constants.Type.back
+                                ? Camera.Constants.Type.front
+                                : Camera.Constants.Type.back
+                                );
+                                }}
+                                    >
+                                <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
+                                    {" "}
+                                    Flip{" "}
+                                </Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.cameraButton}
+                                onPress={async () => {
+                                if (cameraRef) {
+                                const { uri } = await cameraRef.takePictureAsync();
+                                await MediaLibrary.createAssetAsync(uri);
+                                }
+                                }}
+                                >
+                                <View style={styles.takePhotoOut}>
+                                    <View style={styles.takePhotoInner}>
+                                    </View>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    </Camera>
+                </View>
                 <Text style={styles.editPhoto}>Редагувати фото</Text>
                 <Ionicons name="location-outline" size={24} style={styles.logLocation} />
                 <KeyboardAvoidingView
                     behavior={Platform.OS == "ios" ? "padding" : "height"}
-                >
-                <TextInput
-                    style={styles.input}
-                    placeholderTextColor={'#BDBDBD'}
-                    placeholder="Назва..."
-                    value={name}
-                    textContentType="name"
-                    onChangeText={setName}
-                />
-                <TextInput
-                    style={styles.inputLocation}
-                    placeholderTextColor={'#BDBDBD'}
-                    placeholder="Місцевість..."
-                    value={location}
-                    textContentType="location"
-                    onChangeText={setLocation}
-                />
+                    >
+                    <TextInput
+                        style={styles.input}
+                        placeholderTextColor={'#BDBDBD'}
+                        placeholder="Назва..."
+                        value={name}
+                        textContentType="name"
+                        onChangeText={setName}
+                        />
+                    <TextInput
+                        style={styles.inputLocation}
+                        placeholderTextColor={'#BDBDBD'}
+                        placeholder="Місцевість..."
+                        value={location}
+                        textContentType="location"
+                        onChangeText={setLocation}
+                        />
                 </KeyboardAvoidingView>
                 <Text style={styles.button} onPress={() => navigation.navigate("Home")}>Опублікувати</Text>
                 <Ionicons name="trash-outline" size={24} style={styles.logTrash} />
             </View>                      
-
         </View>
     )
 
@@ -60,6 +117,49 @@ const styles = StyleSheet.create({
         height: 760,
         backgroundColor: 'white',
 
+    },
+
+    cameraContainer: { 
+        flex: 1 
+    },
+
+    camera: { 
+        flex: 1 
+    },
+
+    photoView: {
+        flex: 1,
+        backgroundColor: "transparent",
+        justifyContent: "flex-end",
+    },
+
+    flipContainer: {
+        flex: 0.1,
+        alignSelf: "flex-end",
+    },
+
+    cameraButton: { 
+        alignSelf: "center" 
+    },
+
+    takePhotoOut: {
+        borderWidth: 2,
+        borderColor: "white",
+        height: 50,
+        width: 50,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 50,
+    },
+
+    takePhotoInner: {
+        borderWidth: 2,
+        borderColor: "white",
+        height: 40,
+        width: 40,
+        backgroundColor: "white",
+        borderRadius: 50,
     },
 
     header: {
